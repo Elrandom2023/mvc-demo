4232621a92716dc309377987d9e354df3f324e75
LuisVS <villegasalazar08@gmail.com>
2023-04-03 13:53:51 -0500

    Carga de datos con AJAX terminado

--- controllers/curso.controller.php
+++ controllers/curso.controller.php
@@ -5,19 +5,23 @@ require_once "../models/curso.php";
 
 // Verificar si existe una operacion en curso
 //GET - POST - REQUEST
-if(isset($_POST['operacion'])){
+if (isset($_POST['operacion'])){
 
   // Instancia de la clase Curso
   $curso = new Curso();
 
   //Idenificar la operacion: listar, insertar, eliminar, buscar, etc.
-  if($_POST['operacion'] == 'listar'){
+  if ($_POST['operacion'] == 'listar'){
 
     // Utilizaremos el método definido en la clase 
     $resultado = $curso->listarCursos();
 
     // Enviamos el resultado a la vista como un JSON
     echo json_encode($resultado);
+
+  }
+
+  if ($_POST['operacion'] == 'buscarID'){
 
   }
 
